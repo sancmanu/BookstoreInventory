e7e053e84737ee0d69aefa743f172edcf98b5b0e
Manuel Sanchez <sancmanu@sheridancollege.ca>
2019-08-08 14:40:54 -0400

    FileLayer methods

--- BookStore/src/ca/sheridancollege/bookStore/FileLayer.java
+++ BookStore/src/ca/sheridancollege/bookStore/FileLayer.java
@@ -1,8 +1,62 @@
 package ca.sheridancollege.bookStore;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 /**
- * @author Manuel Sanchez Moyano - Student ID 991545555  
+ * @author Manuel Sanchez Moyano - Student ID 991545555
  */
-public class FileLayer 
+public class FileLayer
 {
+   private FileOutputStream output;
+
+   public FileLayer ()
+   {
+      try {
+         this.output = new FileOutputStream("file.dat");
+      }
+      catch (IOException e) {
+
+      }
+   }
+
+   //Create a file
+   public void createFile () throws IOException
+   {
+
+   }
+
+   //Write a file
+   public void writeFile (Book book)
+   {
+      try {
+         DataOutputStream doutput = new DataOutputStream(output);
+         ArrayList<Book> bookList = new ArrayList<Book>();
+         bookList.add(b1);
+
+         for (Book i : bookList) {
+            System.out.println(i.getBookId());
+            doutput.write(i.getBookId());
+         }
+
+      }
+      catch (IOException e) {
+      }
+   }
+
+   //Read a file
+   public void readFile ()
+   {
+
+   }
+
+   //ToDo: delete
+   public static void main (String[] args) throws Exception
+   {
+      FileLayer fl = new FileLayer();
+      Book b1 = new Book();
+      b1.setBookId(1);
+      fl.writeFile(b1);
+   }
 
 }
